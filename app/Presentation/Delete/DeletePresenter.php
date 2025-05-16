4563a186f48408d0fffa947430e65731b1ac9bb3
Olof Johansson <olof@liberdev.se>
2025-05-16 19:57:33 +0200

    Add functionality for deleting posts

---
+++ app/Presentation/Delete/DeletePresenter.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Presentation\Delete;
+
+use Nette;
+use Nette\Application\UI\Form;
+
+final class DeletePresenter extends Nette\Application\UI\Presenter
+{
+	public function __construct(
+		private Nette\Database\Explorer $database,
+	) {}
+
+  public function startup(): void
+  {
+	  parent::startup();
+
+	  if (!$this->getUser()->isLoggedIn()) {
+		  $this->redirect('Sign:in');
+	  }
+  }
+
+  protected function createComponentDeleteForm(): Form
+  {
+    $form = new Form;
+
+    $form->addText('title', 'The title of the post:')->setDisabled();
+
+    $form->addTextArea('content', 'Content:')->setDisabled();
+
+    $form->addSubmit('send', 'Yes, destroy this post now')->setHtmlAttribute('class', 'btn-danger');
+
+    $form->onSuccess[] = $this->deleteFormSucceeded(...);
+
+    return $form;
+  }
+
+  private function deleteFormSucceeded(array $data): void
+  {
+    $id = $this->getParameter('id');
+
+    if ($id) {
+		  $post = $this->database
+			  ->table('posts')
+			  ->get($id);
+
+      $post->delete();
+
+      $this->flashMessage('Post deleted.', 'success');
+      $this->redirect('Home:');
+    }
+  }
+
+  public function renderDelete(int $id): void
+  {
+    $post = $this->database
+      ->table('posts')
+  		->get($id);
+
+  	if (!$post) {
+  		$this->error('Post not found');
+  	}
+
+  	$this->getComponent('deleteForm')
+  		->setDefaults($post->toArray());
+  }
+}
+
