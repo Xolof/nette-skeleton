69efd5fdf4297262fa062dbe6c731b1fc57b2e34
Olof Johansson <olof@liberdev.se>
2025-05-17 12:24:36 +0200

    Add some more tools to test script

--- tests.sh
+++ tests.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/bash
-vendor/bin/phpstan analyse app
+vendor/bin/php-cs-fixer fix app/Model app/Presentation
+
+vendor/bin/phpcs app --ignore=app/Presentation/Accessory/*,app/Presentation/Error/*,app/Bootstrap.php,app/Core/* -s
+vendor/bin/phpcbf app --ignore=app/Presentation/Accessory/*,app/Presentation/Error/*,app/Bootstrap.php,app/Core/* -s
+
+vendor/bin/phpmd app/Model text cleancode
+vendor/bin/phpmd app/Presentation text cleancode
+
+vendor/bin/phpstan analyse app/Model app/Presentation --memory-limit="512M"
+
 php code-checker/code-checker -f
+
 cd cypress/ && npx cypress run; cd -
